ebec4301c860252502c1beac3afc23379ec92912
mimosafa <mimosafa@gmail.com>
2020-02-26 16:41:25 +0900

    add prefecture domain models and test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,29 +2,58 @@
 
 use DoSystem\Application;
 use DoSystem\Domain;
+use DoSystem\Infrastructure;
 use DoSystemMock\Application as MockApplication;
 use DoSystemMock\InMemoryInfrastructure as MockInMemoryInfrastructure;
 
 /**
  * Repositories
  */
-doSystem()->singleton(Domain\Car\Model\CarRepositoryInterface::class, MockInMemoryInfrastructure\Repository\CarRepositoryMock::class);
-doSystem()->singleton(Domain\Vendor\Model\VendorRepositoryInterface::class, MockInMemoryInfrastructure\Repository\VendorRepositoryMock::class);
+doSystem()->singleton(
+    Domain\Car\Model\CarRepositoryInterface::class,
+    MockInMemoryInfrastructure\Repository\CarRepositoryMock::class
+);
+doSystem()->singleton(
+    Domain\Prefecture\Model\PrefectureRepositoryInterface::class,
+    Infrastructure\Repository\PrefectureRepository::class
+);
+doSystem()->singleton(
+    Domain\Vendor\Model\VendorRepositoryInterface::class,
+    MockInMemoryInfrastructure\Repository\VendorRepositoryMock::class
+);
 
 /**
  * Get service application outputs
  */
-doSystem()->bind(Application\Car\Data\GetCarOutputInterface::class, MockApplication\Car\Data\GetCarOutputMock::class);
-doSystem()->bind(Application\Vendor\Data\GetVendorOutputInterface::class, MockApplication\Vendor\Data\GetVendorOutputMock::class);
+doSystem()->bind(
+    Application\Car\Data\GetCarOutputInterface::class,
+    MockApplication\Car\Data\GetCarOutputMock::class
+);
+doSystem()->bind(
+    Application\Vendor\Data\GetVendorOutputInterface::class,
+    MockApplication\Vendor\Data\GetVendorOutputMock::class
+);
 
 /**
  * Query service application filters
  */
-doSystem()->bind(Application\Car\Data\QueryCarFilterInterface::class, MockApplication\Car\Data\QueryCarFilterMock::class);
-doSystem()->bind(Application\Vendor\Data\QueryVendorFilterInterface::class, MockApplication\Vendor\Data\QueryVendorFilterMock::class);
+doSystem()->bind(
+    Application\Car\Data\QueryCarFilterInterface::class,
+    MockApplication\Car\Data\QueryCarFilterMock::class
+);
+doSystem()->bind(
+    Application\Vendor\Data\QueryVendorFilterInterface::class,
+    MockApplication\Vendor\Data\QueryVendorFilterMock::class
+);
 
 /**
  * Query service application outputs
  */
-doSystem()->bind(Application\Car\Data\QueriedCarOutputInterface::class, MockApplication\Car\Data\QueriedCarOutputMock::class);
-doSystem()->bind(Application\Vendor\Data\QueriedVendorOutputInterface::class, MockApplication\Vendor\Data\QueriedVendorOutputMock::class);
+doSystem()->bind(
+    Application\Car\Data\QueriedCarOutputInterface::class,
+    MockApplication\Car\Data\QueriedCarOutputMock::class
+);
+doSystem()->bind(
+    Application\Vendor\Data\QueriedVendorOutputInterface::class,
+    MockApplication\Vendor\Data\QueriedVendorOutputMock::class
+);
