1deb50fd8ca3da277a00faa373306938f2a8354c
mimosafa <mimosafa@gmail.com>
2020-03-25 19:00:15 +0900

    update pseudo database to allow inner joins of tables

--- packages/mimosafa/PseudoDatabase/Table.php
+++ packages/mimosafa/PseudoDatabase/Table.php
@@ -12,6 +12,13 @@ class Table
     private $name;
 
     /**
+     * Table definitions
+     *
+     * @var array
+     */
+    private $definitions;
+
+    /**
      * Data of table
      *
      * @var array[]
@@ -79,6 +86,13 @@ class Table
     private $wheres = [];
 
     /**
+     * Requested select
+     *
+     * @var mixed[]
+     */
+    private $selects = [];
+
+    /**
      * Requested order (by)
      *
      * @var array
@@ -90,9 +104,11 @@ class Table
      * If 'asc', last. If 'desc', first.
      * If not set depends system.
      *
-     * @var string|null
+     * @var array{
+     *      @type string ${$key}
+     * }
      */
-    private $isNullOrder;
+    private $isNullOrder = [];
 
     /**
      * Requested limit results
@@ -107,6 +123,31 @@ class Table
      * @var int
      */
     private $offset = 0;
+
+    /**
+     * Names of table to join
+     *
+     * @var array{
+     *      @type array{
+     *          @type Table  $table
+     *          @type string $join  inner|left|right
+     *          @type string $here
+     *          @type string $compare
+     *          @type string $there
+     *      } ${tableName}
+     * }
+     */
+    private $tablesToJoin = [];
+
+    /**
+     * Acceptable compare string
+     *
+     * @static
+     * @var string[]
+     */
+    private static $acceptableCompareStrings = [
+        '=', 'in', 'like',
+    ];
 
     /**
      * Constructor
@@ -135,6 +176,8 @@ class Table
 
             $this->types[$key] = $definition['type'] ?? 'string';
         }
+
+        $this->definitions = $definitions;
     }
 
     /**
@@ -194,15 +237,41 @@ class Table
             $where =& $this->wheres[\count($this->wheres) - 1];
         }
 
-        if (\in_array($key, $this->keys, true)) {
+        if (\is_string($key) && \in_array($key, $this->keys, true)) {
             if (\count($params) === 2) {
-                $where[] = [$key, $params[0], $params[1]];
+                $compare = \strtolower($params[0]);
+                if (\in_array($compare, self::$acceptableCompareStrings, true)) {
+                    $where[] = [$key, $params[0], $params[1]];
+                }
             }
             else if (\count($params) === 1) {
                 $where[] = [$key, '=', $params[0]];
             }
         }
 
+        return $this;
+    }
+
+    /**
+     * Select
+     *
+     * @param string[] ...$columns
+     * @return self
+     */
+    public function select(string ...$columns): self
+    {
+        foreach ($columns as $column) {
+            if (\strpos($column, ' as ')) {
+                [$column, $key] = \explode(' as ', $column);
+            }
+            else {
+                $key = $column;
+            }
+            if (isset($this->selects[$key])) {
+                throw new \Exception('Invalid argument: cannot select duplicated key `' . $key . '`');
+            }
+            $this->selects[$key] = $column;
+        }
         return $this;
     }
 
@@ -216,7 +285,7 @@ class Table
     public function update(array $data): void
     {
         if (empty($this->wheres)) {
-            throw new \Exception('When using ' . __METHOD__ . ', need to limit target by `where` method.');
+            throw new \Exception('To exec ' . __METHOD__ . ', need to limit target by `where` method.');
         }
 
         foreach ($this->rows as $index => &$row) {
@@ -309,7 +378,7 @@ class Table
         for ($i = 0; $i < count($arguments); $i = $i + 2) {
             $key = $arguments[$i];
             $order = \strtolower($arguments[$i + 1] ?? 'asc');
-            if (\in_array($key, $this->keys, true) && \in_array($order, ['asc', 'desc'], true)) {
+            if (\in_array($order, ['asc', 'desc'], true)) {
                 $this->orderBy[] = [$key, $order];
             }
         }
@@ -324,9 +393,82 @@ class Table
      */
     public function isNull(string $order): self
     {
-        $order = \strtolower($order);
-        if (\in_array($order, ['asc', 'desc'], true)) {
-            $this->isNullOrder = $order;
+        if (!empty($this->orderBy) && $key = $this->orderBy[count($this->orderBy) - 1][0]) {
+            $order = \strtolower($order);
+            if (\in_array($order, ['asc', 'desc'], true)) {
+                $this->isNullOrder[$key] = $order;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Join table
+     *
+     * @param string|self $table
+     * @param string $here
+     * @param string $compare
+     * @param mixed  $there
+     * @return self
+     */
+    public function join($table, string $here, string $compare, $there): self
+    {
+        return $this->innerJoinTables($table)->on($here, $compare, $there);
+    }
+
+    /**
+     * Inner join tables
+     *
+     * @param string[]|self[] ...$tables
+     * @return self
+     */
+    public function innerJoinTables(...$tables): self
+    {
+        static $emptyArray = [
+            'table'   => null,
+            'join'    => 'inner',
+            'here'    => null,
+            'compare' => null,
+            'there'   => null
+        ];
+
+        foreach ($tables as $table) {
+            if ($table instanceof self) {
+                $this->tablesToJoin[$table->name] = $emptyArray;
+                $this->tablesToJoin[$table->name]['table'] = $table;
+            }
+            else if (\is_string($table) && Database::exists($table)) {
+                $this->tablesToJoin[$table] = $emptyArray;
+                $this->tablesToJoin[$table]['table'] = Database::table($table);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Where on joined table
+     *
+     * @param string $here
+     * @param string $compare
+     * @param string $thereKey
+     * @return self
+     */
+    public function on(string $here, string $compare, string $there): self
+    {
+        $theres = explode('.', $there);
+        if (\in_array($here, $this->keys, true) && \in_array($compare, self::$acceptableCompareStrings, true) && count($theres) === 2) {
+            [$name, $key] = $theres;
+            if (isset($this->tablesToJoin[$name]) && $table = $this->tablesToJoin[$name]['table']) {
+                if (\in_array($key, $table->keys, true)) {
+                    $this->tablesToJoin[$name]['here']    = $here;
+                    /**
+                     * @todo Supported only `=`
+                     */
+                    $this->tablesToJoin[$name]['compare'] = $compare;
+                    $this->tablesToJoin[$name]['there'] = $key;
+                }
+            }
         }
         return $this;
     }
@@ -343,21 +485,20 @@ class Table
     }
 
     /**
-     * Get
+     * Get results
      *
      * @return array
      */
     public function get(): array
     {
-        if (empty($this->wheres)) {
-            $results = $this->rows;
+        if (!empty($this->tablesToJoin)) {
+            return $this->joinedTable()->get();
         }
-        else {
-            $results = [];
-            foreach ($this->rows as $index => $row) {
-                if ($row = $this->rowFilter($index, $row)) {
-                    $results[] = $row;
-                }
+
+        $results = [];
+        foreach ($this->rows as $index => $row) {
+            if ($row = $this->rowFilter($index, $row)) {
+                $results[] = $row;
             }
         }
 
@@ -370,24 +511,31 @@ class Table
 
         if (!empty($results) && !empty($this->orderBy)) {
             $orderBys = \array_reverse($this->orderBy);
+            /**
+             * @todo Not work later than 2nd
+             */
             foreach ($orderBys as $orderBy) {
                 $key = $orderBy[0];
+                if (!\in_array($key, $this->keys, true) && !$key = $this->selects[$key] ?? false) {
+                    continue;
+                }
                 $order = \strtolower($orderBy[1]);
                 if (!\in_array($order, ['asc', 'desc'], true)) {
                     continue;
                 }
                 $valueType = $this->types[$key];
-                $sortNull = \in_array($key, $this->nullables, true) && isset($this->isNullOrder);
+                $sortNull = \in_array($key, $this->nullables, true) && isset($this->isNullOrder[$key]);
                 // var_dump(\array_column($results, $key));
                 \usort($results, function ($result1, $result2) use ($key, $order, $valueType, $sortNull) {
                     $value1 = $result1[$key];
                     $value2 = $result2[$key];
                     if ($sortNull) {
+                        $asc = ($order === 'asc') && ($this->isNullOrder[$key] === 'asc');
                         if ($value1 === null) {
-                            return $this->isNullOrder === 'asc' ? 1 : -1;
+                            return $asc ? 1 : -1;
                         }
                         else if ($value2 === null) {
-                            return $this->isNullOrder === 'asc' ? -1 : 1;
+                            return $asc ? -1 : 1;
                         }
                     }
                     if ($valueType === 'integer') {
@@ -407,6 +555,10 @@ class Table
             }
         }
 
+        $results = \array_map(function ($row) {
+            return $this->columnFilter($row);
+        }, $results);
+
         $this->initRequests();
 
         return $results;
@@ -423,7 +575,10 @@ class Table
      */
     private function rowFilter(int $index, array $row): ?array
     {
-        return $this->execWhere($index, $row) ? $this->columnFilter($row) : null;
+        if (empty($this->wheres)) {
+            return $row;
+        }
+        return $this->execWhere($index, $row) ? $row : null;
     }
 
     /**
@@ -495,9 +650,103 @@ class Table
      */
     private function columnFilter(array $row): array
     {
-        //
+        if (empty($this->selects)) {
+            return $row;
+        }
+        $filteredRow = [];
+        foreach ($this->selects as $key => $column) {
+            $filteredRow[$key] = !isset($this->keys[$column]) ? $row[$column] : null;
+        }
+        return $filteredRow;
+    }
 
-        return $row;
+    /**
+     * Get joined table instance
+     *
+     * @access private
+     *
+     * @return self
+     */
+    public function joinedTable(): self
+    {
+        $name = $this->name;
+        $definitions = $this->definitions;
+        $rows = $this->rows;
+
+        $joinedRows = [];
+
+        foreach ($this->tablesToJoin as $tableName => $array) {
+            $table = $array['table'];
+
+            $hereKey = $array['here'];
+            $compare = $array['compare'];
+            $thereKey = $array['there'];
+
+            if (\is_null($hereKey) || \is_null($compare) || \is_null($thereKey)) {
+                continue;
+            }
+
+            if ($compare !== '=') {
+                /**
+                 * @todo
+                 */
+                continue;
+            }
+
+            $tableKeys = $table->keys;
+            $tableDefinitions = $table->definitions;
+
+            $name = "{$name}.{$tableName}";
+
+            $joinedKeys = [];
+            foreach ($tableKeys as $tableKey) {
+                $joinedKey = "{$tableName}.{$tableKey}";
+                $definitions[$joinedKey] = $tableDefinitions[$tableKey];
+                $joinedKeys[] = $joinedKey;
+            }
+
+            $join = $array['join'];
+            $tableRows = $table->rows;
+            $tableIndexes = \array_column($tableRows, $thereKey);
+
+            if ($join === 'inner') {
+                foreach ($rows as $row) {
+                    $hereValue = $row[$hereKey];
+                    if ($compare === '=') {
+                        $tableIndex = \array_search($hereValue, $tableIndexes, true);
+                        if ($tableIndex !== false) {
+                            $tableRow = $tableRows[$tableIndex];
+                            $joinedRow = $row;
+                            foreach ($tableKeys as $i => $tableKey) {
+                                $joinedRow[$joinedKeys[$i]] = $tableRow[$tableKey];
+                            }
+                            $joinedRows[] = $joinedRow;
+                        }
+                    }
+                    else {
+                        /** @todo */
+                    }
+                }
+            }
+        }
+
+        $joinedTable = new self($name, $definitions);
+        // Recover primary key
+        $joinedTable->primary = $this->primary;
+        $joinedTable->rows = $joinedRows;
+        $joinedTable->total = count($joinedTable->rows);
+        $joinedTable->wheres = $this->wheres;
+        $joinedTable->selects = $this->selects;
+        $joinedTable->orderBy = $this->orderBy;
+        $joinedTable->isNullOrder = $this->isNullOrder;
+        $joinedTable->limit = $this->limit;
+        $joinedTable->offset = $this->offset;
+
+        $this->initRequests();
+
+        # var_dump($joinedTable);
+
+        return $joinedTable;
     }
 
     /**
@@ -523,9 +772,11 @@ class Table
     public function initRequests(): void
     {
         $this->wheres = [];
+        $this->selects = [];
         $this->orderBy = [];
-        $this->isNullOrder = null;
+        $this->isNullOrder = [];
         $this->limit = 0;
         $this->offset = 0;
+        $this->tablesToJoin = [];
     }
 }
